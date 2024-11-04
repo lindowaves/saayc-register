50910ca7be2ccf4069234a097b832397a526fe01
kamogelo0010 <brianshongwe777@gmail.com>
2024-11-04 13:57:07 +0200

    created backend and fronend

---
+++ client/src/App.jsx
@@ -0,0 +1,85 @@
+import React, {useState, useEffect} from 'react'
+import './App.css'
+import Axios from "axios";
+import Card from "./components/card";
+
+function App() {
+
+    const baseUrl = "http://localhost:3001"
+
+    const [values, setValues] = useState();
+    const [games, setGames] = useState();
+
+    const handleChangeValues = (value) => {
+        setValues((prevValue) => ({
+            ...prevValue,
+            [value.target.name]: value.target.value,
+        }))
+    }
+
+    const handleClickButton = () => {
+        Axios.post(`${baseUrl}/register`, {
+            name: values.name,
+            surname: values.surname,
+            age: values.age,
+            gender: values.gender,
+            contact: values.contact,
+            reason: values.reason,
+            timein: values.timein,
+            timeout: values.timeout,
+        }).then((response) =>{
+            console.log(response)
+        });
+    }
+
+    useEffect(() => {
+        Axios.get(`${baseUrl}/games`)
+            .then((response)=>{
+            setGames(response.data)
+        })
+        }
+    )
+
+
+  return (
+    <div className="App">
+      <div className="container">
+          <h1 className="title">SAAYC ETWATWA PORTAL</h1>
+<br/>
+          <div className="register-box">
+              <input className="register-input" type="text" name="name" placeholder="name" onChange={handleChangeValues} />
+              <input className="register-input" type="text" name="surname" placeholder="surname" onChange={handleChangeValues} />
+              <input className="register-input" type="text" name="age" placeholder="age" onChange={handleChangeValues} />
+
+              <input className="register-input" type="text" name="gender" placeholder="gender" onChange={handleChangeValues} />
+              <input className="register-input" type="text" name="contact" placeholder="contact" onChange={handleChangeValues} />
+              <input className="register-input" type="text" name="reason" placeholder="reason" onChange={handleChangeValues} />
+              <input className="register-input" type="text" name="timein" placeholder="timein" onChange={handleChangeValues} />
+              <input className="register-input" type="text" name="timeout" placeholder="timeout" onChange={handleChangeValues} />
+              <button type='submit' className="register-button" onClick={handleClickButton}>Sign Register</button>
+          </div>
+          <br/>
+          <div className="cards">
+              {typeof games !== 'undefined' &&
+                  games.map((game) => {
+                      return <Card
+                          key={game.idgames}
+                          id={game.idgames}
+                          name={game.name}
+                          surname={game.surname}
+                          age={game.age}
+                          gender={game.gender}
+                          contact={game.contact}
+                          reason={game.reason}
+                          timein={game.timein}
+                          timeout={game.timeout}
+                      >
+                      </Card>;
+                  })}
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const server = express();
+const mysql = require('mysql');
+const cors = require('cors');
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "test",
+});
+
+server.use(express.json());
+server.use(cors());
+
+server.post("/register", (req, res) => {
+    const { name } = req.body;
+    const { surname } = req.body;
+    const { age } = req.body;
+    const { gender } = req.body;
+    const { contact } = req.body;
+    const { reason } = req.body;
+    const { timein } = req.body;
+    const { timeout } = req.body;
+
+    let sql = "INSERT INTO register (name, surname, age, gender, contact, reason, timein, timeout) VALUES (?,?,?,?,?,?,?,?)"
+    db.query(sql, [name, surname, age, gender, contact, reason, timein, timeout], (err,result) =>{
+        if (err) {
+            console.log(err);
+        }else{
+            console.log(result);
+        }
+    })
+});
+
+server.get("/games", (req, res) => {
+
+    let sql = "SELECT * FROM register";
+    db.query(sql, (err,result) =>{
+        if (err) {
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+
+    })
+});
+
+
+
+
+server.listen(3001, () =>
+    console.log("Running in the port 3001")
+);
