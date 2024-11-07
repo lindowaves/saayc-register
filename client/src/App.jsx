d2244ff471824466b1dd1bbcf351b435ff5efb2d
lindowaves <botsalomolatsile@gmail.com>
2024-11-07 19:33:34 +0200

    Deleted Card.jsx and Cardlist.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Axios from 'axios';
-import Card from './components/Card';
 import RegisterForm from './components/RegisterForm';
-import CardList from './components/CardList';
 import DataTable from './components/Columns';
 
 function App() {
